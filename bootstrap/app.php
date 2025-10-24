60f0f2400f2931ba62fc4fd3d66c5cedb35239db
eyuel <eyuelalemnew227@gmail.com>
2025-10-24 10:14:03 +0300

    Register Spatie permission/role middlewares (permission, role, role_or_permission) in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -26,6 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'powerbi' => PowerBiApiKeyMiddleware::class,
+            'permission' => PermissionMiddleware::class,
+            'role' => RoleMiddleware::class,
+            'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
